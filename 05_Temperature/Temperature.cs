b96617f61b86a40b29616e97dff90a0a59dd232b
Sez <Sez@LENOVO-PC>
2016-09-15 11:24:57 +0200

    Add project files.

---
+++ 05_Temperature/Temperature.cs
@@ -0,0 +1,44 @@
+﻿namespace _05_Temperature
+{
+        public enum Unit
+        {
+            Fahrenheit,
+            Celcius
+        };
+
+        public class Temperature
+        {
+            public int Fahrenheit;
+            public int Celcius;
+
+            public Temperature(Unit unit, int input)
+            {
+                Fahrenheit = input;
+                Celcius = input;
+            }
+            public Temperature()
+            {
+
+            }
+
+            internal static double FahrenheitToCelcius(double x)
+            {
+                //double y = (x - 32) / 1.80000;
+                //string s = y.ToString().Replace('.', ',');
+                //return Convert.ToDouble(s);
+
+                double y = (x - 32) * 5 / 9;
+                return y;
+            }
+
+            internal static double CelciusToFahrenheit(double v)
+            {
+                //double y = v * 5 / 9 + 32;
+                //string s = y.ToString().Replace('.', ',');
+                //return Convert.ToDouble(s);
+
+                double y = v * 9 / 5 + 32;
+                return y;
+            }
+        }
+    }
